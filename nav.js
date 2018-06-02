ef625c3baf22820576fa3fdaca8c8e4a2766fd50
Udit Narayan Sia <udit.sia@gmail.com>
2018-06-02 20:42:12 +0530

    nav beta

--- nav.js
+++ nav.js
@@ -1,15 +1,26 @@
 window.onload = init;
 function init() {
         document.querySelector("#branches").addEventListener("mouseover",dealMouseB);
-        var branches =  document.querySelector("#navSec");
+        document.querySelector("#Specialty").addEventListener("mouseover",dealMouseS);
+        document.querySelector("#home").addEventListener("mouseover",dealMouseBody);
+        document.querySelector("#about").addEventListener("mouseover",dealMouseBody);
+        document.querySelector("#departments").addEventListener("mouseover",dealMouseBody);
+        document.querySelector("#contactUs").addEventListener("mouseover",dealMouseBody);
+        var navSec =  document.querySelector("#navSec");
         function dealMouseB() {
 
-            branches.style="height:300px";
-            branches.innerHTML="<ul> <li>Bhubaneswar</li><li>Cuttack</li><li>Roulkela</li></ul>";
+            navSec.style="height:300px";
+            navSec.innerHTML="<ul> <li>Bhubaneswar</li><li>Kolkata</li><li>Hyderabad</li></ul>";
 
         }
+        function dealMouseS() {
+            navSec.style="height:300px";
+            navSec.innerHTML="<ul> <li>Orthopaedics & Joint Replacement</li><li>Blood Bank & Transfusion Medicine</li><li>Cardiac Sciences</li><li>Dentistry & Maxillo Facial Surgery</li>" +
+                "<li>Dermatology</li><li>Emergency & Critical Care</li><li>Ent & Head Neck Surgery</li><li>Radiology & Interventional Radiology</li><li>CGynaecology & Obstetrics</li></ul>";
+        }
+
     document.querySelector("body").addEventListener("click",dealMouseBody);
         function dealMouseBody() {
-            branches.style="height:0px";
+            navSec.style="height:0px";
         }
 }
